57e7f19f904f431038b97a2f4a9a031b06c14a56
AlexRFarnes <alexrfarnes@gmail.com>
2026-04-23 06:45:44 +0800

    patron composite

--- 02-estructurales/03-composite.ts
+++ 02-estructurales/03-composite.ts
@@ -13,3 +13,68 @@
  * https://refactoring.guru/es/design-patterns/composite
  *
  */
+
+import { COLORS } from "../helpers/colors.ts";
+
+interface FileSystemComponent {
+  showDetails(indent?: string): void;
+}
+
+class File implements FileSystemComponent {
+  private name: string;
+
+  constructor(name: string) {
+    this.name = name;
+  }
+
+  showDetails(indent?: string): void {
+    console.log(`%c${indent}- Archivo: ${this.name}`, COLORS.green);
+  }
+}
+
+class Directory implements FileSystemComponent {
+  private name: string;
+  private content: FileSystemComponent[] = [];
+
+  constructor(name: string) {
+    this.name = name;
+  }
+
+  showDetails(indent: string = ""): void {
+    console.log(`%c${indent}+ Directorio: ${this.name}`, COLORS.blue);
+    this.content.forEach(component => component.showDetails(indent + " "));
+  }
+
+  add(component: FileSystemComponent): void {
+    this.content.push(component);
+  }
+}
+
+function main() {
+  const file1 = new File("archivo1.txt");
+  const file2 = new File("archivo2.txt");
+  const file3 = new File("archivo3.txt");
+  const file4 = new File("archivo4.txt");
+  const file5 = new File("archivo5.txt");
+
+  const dir1 = new Directory("directorio1");
+  const dir2 = new Directory("directorio2");
+  const dir3 = new Directory("directorio3");
+
+  dir1.add(file1);
+  dir1.add(file2);
+
+  dir2.add(file3);
+  dir2.add(dir3);
+
+  dir3.add(file4);
+
+  const root = new Directory("root");
+  root.add(dir1);
+  root.add(dir2);
+  root.add(file5);
+
+  root.showDetails();
+}
+
+main();
