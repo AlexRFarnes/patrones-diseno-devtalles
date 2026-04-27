92ec9453538f387913bae937baa18391b96dbcab
Alex R. Farnes <alexrfarnes@gmail.com>
2026-04-27 13:35:23 +0800

    patron flyweight

--- 02-estructurales/06-flyweight.ts
+++ 02-estructurales/06-flyweight.ts
@@ -8,3 +8,69 @@
  *
  * https://refactoring.guru/es/design-patterns/flyweight
  */
+
+import { COLORS } from "../helpers/colors.ts";
+
+interface Location {
+  display(coordinates: { x: number; y: number }): void;
+}
+
+// Flyweight
+class LocationIcon implements Location {
+  private type: string; // "house", "school", "hospital", etc.
+  private iconImage: string; // "house.png", "school.png", "hospital.png", etc.
+
+  constructor(type: string, iconImage: string) {
+    this.type = type;
+    this.iconImage = iconImage;
+  }
+
+  display(coordinates: { x: number; y: number }): void {
+    console.log(
+      `Coordenadas: ${this.type} ${coordinates.x}, ${coordinates.y} con icono %c[${this.iconImage}]`,
+      COLORS.blue,
+    );
+  }
+}
+
+// Factory Flyweight
+class LocationFactory {
+  private icons: Record<string, LocationIcon> = {};
+
+  getLocationIcon(type: string): LocationIcon {
+    if (!this.icons[type]) {
+      console.log(`%cCreando icono de ${type}`, COLORS.red);
+      const iconImage = `imagen_de_${type.toLowerCase()}.png`;
+      this.icons[type] = new LocationIcon(type, iconImage);
+    }
+    return this.icons[type];
+  }
+}
+
+class MapLocation {
+  private coordinates: { x: number; y: number };
+  private icon: LocationIcon;
+
+  constructor(coordinates: { x: number; y: number }, icon: LocationIcon) {
+    this.coordinates = coordinates;
+    this.icon = icon;
+  }
+
+  display(): void {
+    this.icon.display(this.coordinates);
+  }
+}
+
+function main() {
+  const factory = new LocationFactory();
+  const locations = [
+    new MapLocation({ x: 10, y: 20 }, factory.getLocationIcon("hospital")),
+    new MapLocation({ x: -10, y: -20 }, factory.getLocationIcon("hospital")),
+    new MapLocation({ x: 352, y: 100 }, factory.getLocationIcon("escuela")),
+    new MapLocation({ x: -100, y: -200 }, factory.getLocationIcon("escuela")),
+  ];
+
+  locations.forEach(location => location.display());
+}
+
+main();
