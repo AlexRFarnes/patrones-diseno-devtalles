13cfc3e23a65272a712eee28aea31ec21ba97351
Alex R. Farnes <alexrfarnes@gmail.com>
2026-04-28 13:24:50 +0800

    patron proxy

--- 02-estructurales/07-proxy.ts
+++ 02-estructurales/07-proxy.ts
@@ -10,3 +10,54 @@
  * https://refactoring.guru/es/design-patterns/proxy
  *
  */
+
+import { COLORS } from "../helpers/colors.ts";
+
+class Player {
+  name: string;
+  level: number;
+
+  constructor(name: string, level: number) {
+    this.name = name;
+    this.level = level;
+  }
+}
+
+interface Room {
+  enter(player: Player): void;
+}
+
+class SecretRoom implements Room {
+  enter(player: Player): void {
+    console.log(`%c${player.name} ha entrado a la sala secreta`, COLORS.green);
+  }
+}
+
+class MagicPortal implements Room {
+  private secretRoom: Room;
+
+  constructor(secretRoom: Room) {
+    this.secretRoom = secretRoom;
+  }
+
+  enter(player: Player): void {
+    if (player.level >= 10) {
+      this.secretRoom.enter(player);
+      return;
+    }
+    console.log(
+      `%c${player.name} no tiene nivel suficiente para entrar a la sala secreta`,
+      COLORS.red,
+    );
+  }
+}
+
+function main() {
+  const magicPortal = new MagicPortal(new SecretRoom());
+  const player = new Player("John", 10);
+  magicPortal.enter(player);
+  const player2 = new Player("Jane", 5);
+  magicPortal.enter(player2);
+}
+
+main();
