3d2c30180972a2750e0477dfa85953836f512fd6
AlexRFarnes <alexrfarnes@gmail.com>
2026-05-02 06:59:29 +0800

    patron strategy

--- 03-comportamiento/08-strategy.ts
+++ 03-comportamiento/08-strategy.ts
@@ -17,3 +17,67 @@
  * ! patitos compiten en una carrera y cada uno tiene su propia
  * ! estrategia de movimiento (por ejemplo, nadar, volar o caminar).
  */
+
+import { COLORS } from "./../helpers/colors.ts";
+
+interface MovingStrategy {
+  move(): void;
+}
+
+class Swim implements MovingStrategy {
+  move(): void {
+    console.log(`\n%cEl pato vuela elegantemente sobre el agua`, COLORS.blue);
+  }
+}
+
+class Fly implements MovingStrategy {
+  move(): void {
+    console.log(`\n%cEl pato vuela elegantemente sobre el agua`, COLORS.pink);
+  }
+}
+
+class Walk implements MovingStrategy {
+  move(): void {
+    console.log(`\n%cEl pato camina torpemente por la orilla`, COLORS.green);
+  }
+}
+
+class Duck {
+  private name: string;
+  private movingStrategy: MovingStrategy;
+
+  constructor(name: string, movingStrategy: MovingStrategy) {
+    this.name = name;
+    this.movingStrategy = movingStrategy;
+
+    console.log(`%c${name} está listo para competir\n\n`, COLORS.yellow);
+  }
+
+  move(): void {
+    console.log(`\n%c${this.name} se prepara para moverse`, COLORS.yellow);
+    this.movingStrategy.move();
+  }
+
+  setMovingStrategy(movingStrategy: MovingStrategy): void {
+    this.movingStrategy = movingStrategy;
+    console.log(
+      `\n%c${this.name} ha cambiado su estrategia de movimiento`,
+      COLORS.yellow,
+    );
+  }
+}
+
+function main() {
+  const duck1 = new Duck("Donald", new Swim());
+  const duck2 = new Duck("Daffy", new Fly());
+  const duck3 = new Duck("Scrooge", new Walk());
+
+  duck1.move();
+  duck2.move();
+  duck3.move();
+
+  duck3.setMovingStrategy(new Swim());
+  duck3.move();
+}
+
+main();
