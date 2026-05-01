4fbf20909d8c080e7450f615e5bc879c4d21d744
AlexRFarnes <alexrfarnes@gmail.com>
2026-05-02 07:20:32 +0800

    patron template method

--- 03-comportamiento/09-template-method.ts
+++ 03-comportamiento/09-template-method.ts
@@ -26,3 +26,57 @@
  * ya que define un esqueleto general del algoritmo en una clase base
  * y delega los detalles específicos a las subclases.
  */
+
+import { COLORS } from "./../helpers/colors.ts";
+
+abstract class HotBeverage {
+  prepare(): void {
+    this.boilWater();
+    this.addMainIngredient();
+    this.pourInCup();
+    this.addCondiments();
+  }
+
+  private boilWater(): void {
+    console.log("%cHirviendo agua", COLORS.red);
+  }
+
+  private pourInCup(): void {
+    console.log("%cVertiendo en una taza", COLORS.blue);
+  }
+
+  protected abstract addMainIngredient(): void;
+  protected abstract addCondiments(): void;
+}
+
+class Tea extends HotBeverage {
+  protected override addMainIngredient(): void {
+    console.log("%cAñadiendo una bolsa de té", COLORS.green);
+  }
+
+  protected override addCondiments(): void {
+    console.log("%cAñadiendo miel y limón", COLORS.yellow);
+  }
+}
+
+class Coffee extends HotBeverage {
+  protected override addMainIngredient(): void {
+    console.log("%cAñadiendo café molido", COLORS.green);
+  }
+
+  protected override addCondiments(): void {
+    console.log("%cAñadiendo azúcar y leche", COLORS.yellow);
+  }
+}
+
+function main() {
+  const tea = new Tea();
+  console.log("%cPreparando té", COLORS.blue);
+  tea.prepare();
+  console.log("\n");
+  console.log("%cPreparando café", COLORS.blue);
+  const coffee = new Coffee();
+  coffee.prepare();
+}
+
+main();
